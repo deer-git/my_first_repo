62c6371b203f58489a2337e4eda960e40eeb5f27
dsr <dsignr39@gmail.com>
2020-11-15 17:54:02 -0600

    Variables, strings

---
+++ clase1.js
@@ -0,0 +1,12 @@
+//podemos no usar ; en js pero suele ser una buena practica excepto casos obligaorios
+var nombre
+nombre = 'Eliot'
+var apellido = 'Lifszyc'
+
+//debilmente tipado
+var edad = 28
+edad = '28 años'
+
+//para conctatenar podemos usar el signo +
+console.log('Bienvenido ' + nombre + ' ' + apellido);
+console.log('Tienes ' + edad);
