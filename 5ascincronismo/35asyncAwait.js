25df11aea9f266d4f3bdb8065b141e6e59d32dfd
dsr <dsignr39@gmail.com>
2020-12-12 16:40:58 -0600

    Async-await: lo ultimo en asincronismo

---
+++ 5ascincronismo/35asyncAwait.js
@@ -0,0 +1,39 @@
+// Clase 35 Async-await lo ultimo en asincronismo
+// ------------(``)-------------
+
+const API_URL = 'https://swapi.dev/api/'
+const PEOPLE_URL = 'people/:id'
+
+const opts = { crossDomain: true}
+
+function obtenerPersonaje(id) {
+    //return new Promise(function (resolve, reject)){}
+    return new Promise((resolve, reject) => {
+        const url = `${API_URL}${PEOPLE_URL.replace(':id', id)}`
+        $
+            .get(url, opts, function (data) {
+                resolve(data)
+            })
+            .fail(() => reject(id))
+            })
+}
+
+function onError(id){
+    console.log(`Sucedio un error al obtener el personaje ${id}`)
+}
+
+//#1
+/* para poder usar  await la funcion tiene que ser async */
+async function obtenerPersonajes(){
+    var ids = [1,2,3,4,5,6,7]
+    var promesas = ids.map( id => obtenerPersonaje(id))
+    try {
+        var personajes = await Promise.all(promesas)
+        console.log(personajes)
+    } catch (id) {
+        onError(id)
+    }
+}
+
+obtenerPersonajes()
+
